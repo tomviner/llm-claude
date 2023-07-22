fa85f77ee78540ee662017a4066756bca7a4dd01
Tom Viner <tom@viner.tv>
2023-07-22 15:57:01 +0100

    use anthropic library

---
+++ llm_claude/__init__.py
@@ -0,0 +1,54 @@
+import llm
+import httpx
+import click
+
+from anthropic import Anthropic, HUMAN_PROMPT, AI_PROMPT
+
+
+@llm.hookimpl
+def register_models(register):
+    # https://docs.anthropic.com/claude/reference/selecting-a-model
+    # Family           Latest major version    Latest full version
+    # Claude Instant   claude-instant-1        claude-instant-1.1
+    # Claude           claude-2                claude-2.0
+    register(Claude("claude-instant-1"), aliases=("claude-instant",))
+    register(Claude("claude-2"), aliases=("claude",))
+
+
+class Claude(llm.Model):
+    needs_key = "claude"
+    key_env_var = "ANTHROPIC_API_KEY"
+    can_stream = True
+
+    def __init__(self, model_id):
+        self.model_id = model_id
+
+    def generate_prompt_messages(self, prompt, conversation):
+        if conversation:
+            for response in conversation.responses:
+                yield self.build_prompt(response.prompt.prompt, response.text())
+
+        yield self.build_prompt(prompt)
+
+    def build_prompt(self, human, ai=""):
+        return f"{HUMAN_PROMPT} {human}{AI_PROMPT}{ai}"
+
+    def execute(self, prompt, stream, response, conversation):
+        anthropic = Anthropic(api_key=self.get_key())
+
+        prompt_str = "".join(self.generate_prompt_messages(prompt.prompt, conversation))
+
+        completion = anthropic.completions.create(
+            model=self.model_id,
+            max_tokens_to_sample=300,
+            prompt=prompt_str,
+            stream=stream,
+        )
+        if stream:
+            for comp in completion:
+                yield comp.completion
+        else:
+            yield completion.completion
+
+    def __str__(self):
+        return "Anthropic: {}".format(self.model_id)
---
+++ tests/test_llm_claude.py
@@ -0,0 +1,88 @@
+import ast
+from click.testing import CliRunner
+from unittest.mock import patch, MagicMock, Mock
+from llm.cli import cli
+from llm_claude import Anthropic, Claude
+from llm_claude.vendored_anthropic import Anthropic, HUMAN_PROMPT, AI_PROMPT
+from llm import Prompt, get_model, Response
+import os
+import pytest
+from typing import List, Tuple
+
+
+@patch("llm_claude.Anthropic")
+def test_claude_response(mock_anthropic):
+    mock_response = MagicMock()
+    mock_response.completion = "hello"
+    mock_anthropic.return_value.completions.create.return_value.__iter__.return_value = [
+        mock_response
+    ]
+    prompt = Prompt("hello", "")
+    model = Claude("claude-2")
+    model.key = "key"
+    items = list(model.response(prompt))
+
+    mock_anthropic.return_value.completions.create.assert_called_with(
+        model="claude-2",
+        max_tokens_to_sample=300,
+        prompt="\n\nHuman: hello\n\nAssistant:",
+        stream=True,
+    )
+
+    assert items == ["hello"]
+
+
+@patch.dict(os.environ, {"ANTHROPIC_API_KEY": "X"})
+@patch("llm_claude.Anthropic")
+def test_claude_prompt(mock_anthropic):
+    mock_response = Mock()
+    mock_response.completion = "🐶🐶"
+    mock_anthropic.return_value.completions.create.return_value = mock_response
+    runner = CliRunner()
+    result = runner.invoke(cli, ["two dog emoji", "-m", "claude", "--no-stream"])
+    assert result.exit_code == 0, result.output
+    assert result.output == "🐶🐶\n"
+
+
+@pytest.mark.parametrize(
+    "prompt, conversation_messages, expected",
+    (
+        ("hello", [], [f"{HUMAN_PROMPT} hello{AI_PROMPT}"]),
+        (
+            "hello 2",
+            [("user 1", "response 1")],
+            [
+                f"{HUMAN_PROMPT} user 1{AI_PROMPT}response 1",
+                f"{HUMAN_PROMPT} hello 2{AI_PROMPT}",
+            ],
+        ),
+        (
+            "hello 3",
+            [("user 1", "response 1"), ("user 2", "response 2")],
+            [
+                "\n\nHuman: user 1\n\nAssistant:response 1",
+                "\n\nHuman: user 2\n\nAssistant:response 2",
+                "\n\nHuman: hello 3\n\nAssistant:",
+            ],
+        ),
+    ),
+)
+def test_generate_prompt_messages(
+    prompt: str, conversation_messages: List[Tuple[str, str]], expected: List[str]
+):
+    model = get_model("claude")
+    conversation = None
+    if conversation_messages:
+        conversation = model.conversation()
+        for prev_prompt, prev_response in conversation_messages:
+            conversation.responses.append(
+                Response.fake(
+                    prompt=prev_prompt,
+                    model=model,
+                    system=None,
+                    response=prev_response,
+                )
+            )
+
+    messages = model.generate_prompt_messages(prompt, conversation)
+    assert list(messages) == expected
